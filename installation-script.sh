1a687671075b66bbfd26a1b2d15615b49f8c0935
Cem Subaşı <cemsubasi@live.com>
2024-05-27 00:32:53 +0300

    Init

---
+++ installation-script.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# echo 'alias l="ls"' >> ~/.bashrc
+# source ~/.bashrc
+
+# sudo yum upgrade vim
+# sudo yum install nginx
+# sudo yum install certbot
+#
+# sudo yum install docker
+# sudo groupadd docker
+# sudo usermod -aG docker $USER
+# newgrp docker
+#
+# sudo yum install git
+
+# docker pull avendvine/mysql-dev
+
+# docker run --name mysql -d -p 9900:3306 -v mysql-data:/var/lib/mysql avendvine/mysql
+# docker run --name mysql-prod -d -p 9933:3306 -v mysql-data:/var/lib/mysql avendvine/mysql-dev
